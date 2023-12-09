4decc897551aaed373c1981e9bd678c225e0bda1
unknown <henrique.sousa60@etec.sp.gov.br>
2023-12-09 17:50:53 -0300

    React JavaScript - Projeto V3 - Lista de Tarefa V2

---
+++ src/components/TodoForm.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+
+const TodoForm = ({ addTodo }) => {
+  const [value, setValue] = useState("");
+  const [category, setCategorty] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(!value || !category) return;
+    // Adcionar todo
+    addTodo(value, category);
+
+    // Limpar os campos
+    setValue("")
+    setCategorty("")
+
+
+
+    console.log(value, category);
+  };
+
+  return (
+    <div className='todo-form'>
+        <h2>Criar Tarefa:</h2>
+        <form onSubmit={handleSubmit}>
+            <input 
+            type="text" 
+            placeholder="Digite o título" 
+            value={value}
+            onChange={(e) => setValue(e.target.value)} 
+            />
+            <select value={category} onChange={(e) => setCategorty(e.target.value)} >
+                <option value="">Selecione uma categoria</option>
+                <option value="Trabalho">Trabalho</option>
+                <option value="Pessoal">Pessoal</option>
+                <option value="Estudos">Estudos</option>
+            </select>
+            <button type="submit">Criar Tarefa</button>
+        </form>
+    </div>
+  )
+}
+
+export default TodoForm;
